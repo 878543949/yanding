e2ba8c15a8ff0770036e587ce57b6a0ba861dc3e
keray <11723046452qq.com>
2019-12-26 23:29:37 +0800

    dev

---
+++ src/pages/task/task.vue
@@ -0,0 +1,96 @@
+<template>
+    <view class="task">
+        <view class="task-keep">
+            已守约
+            <text class="keep-num">{{ num }}</text>次
+        </view>
+        <view class="task-doing">进行中({{ doing }})</view>
+        <view class="task-finsh">已完成({{ finish }})</view>
+        <view class="task-fail">未完成({{ fail }})</view>
+        <view class="task-add" @click="goAdd">
+            <text>+</text>
+            <text>约定</text>
+        </view>
+    </view>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            num: 3,
+            doing: 1,
+            finish: 2,
+            fail: 0
+        };
+    },
+    methods: {
+        goAdd() {
+            uni.navigateTo({
+                url: "../index/index"
+            });
+        }
+    }
+};
+</script>
+
+<style scoped lang="scss">
+.task {
+    background: #eee;
+    height: 100%;
+    padding: 10px;
+
+    .task-keep {
+        background: white;
+        text-align: center;
+        color: #666;
+        padding: 10px 0;
+        font-size: 16px;
+        border-bottom: 1px solid #fff;
+        margin-bottom: 8px;
+        border-radius: 5px;
+        .keep-num {
+            font-size: 22px;
+            padding: 0 5px;
+        }
+    }
+    .task-doing {
+        background: white;
+        color: #666;
+        padding: 10px;
+        border-bottom: 1px solid #fff;
+        margin-bottom: 8px;
+        border-radius: 5px;
+    }
+    .task-finsh {
+        background: white;
+        color: #666;
+        padding: 10px;
+        border-bottom: 1px solid #fff;
+        margin-bottom: 8px;
+        border-radius: 5px;
+    }
+    .task-fail {
+        background: white;
+        color: #666;
+        padding: 10px 15px;
+        border-radius: 5px;
+        margin-bottom: 8px;
+    }
+    .task-add {
+        width: 50px;
+        height: 50px;
+        color: white;
+        position: fixed;
+        bottom: 50px;
+        right: 50px;
+        background: #3393f4;
+        border-radius: 50%;
+        text {
+            display: block;
+            text-align: center;
+            font-size: 14px;
+        }
+    }
+}
+</style>
---
+++ src/utils/api.js
@@ -0,0 +1,38 @@
+import { http } from "./request.js"
+
+// 协约类型
+export function agreementList(params) {
+    // const data = qs.stringify(params)
+    return http({
+        url: "/agreement/type",
+        data: params,
+        method: "GET"
+    })
+}
+// 协约次数类型
+export function agreementCountList(params) {
+    // const data = qs.stringify(params)
+    return http({
+        url: "/agreement/type/count",
+        data: params,
+        method: "GET"
+    })
+}
+// 周期类型
+export function agreementCycle(params) {
+    // const data = qs.stringify(params)
+    return http({
+        url: "/agreement/type/cycle",
+        data: params,
+        method: "GET"
+    })
+}
+// 约定周期
+export function agreementCycleTime(params) {
+    // const data = qs.stringify(params)
+    return http({
+        url: "/agreement/type/cycle/time",
+        data: params,
+        method: "GET"
+    })
+}
